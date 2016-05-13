ac4511b605f9e20510711205881bcac5de64c8e9
Claudiu <groza.claudiu9217@gmail.com>
2016-05-13 18:50:42 +0300

    Extract layout dimens

--- app/src/main/java/ro/itec/waity/login/view/LoginActivityView.java
+++ app/src/main/java/ro/itec/waity/login/view/LoginActivityView.java
@@ -3,6 +3,7 @@ package ro.itec.waity.login.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import ro.itec.waity.R;
 import ro.itec.waity.login.LoginMVP;
@@ -14,5 +15,14 @@ public class LoginActivityView extends AppCompatActivity implements LoginMVP.Req
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+        setStatusBarTranslucent(true);
+    }
+
+    protected void setStatusBarTranslucent(boolean makeTranslucent) {
+        if (makeTranslucent) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else {
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 }
