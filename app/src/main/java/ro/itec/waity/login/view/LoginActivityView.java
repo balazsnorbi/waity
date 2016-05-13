2564ca1b23cd215fe29abacd0819230403cc596d
Norbert <balazsnorbi@gmail.com>
2016-05-13 19:40:25 +0300

    Remove Main Activity

--- app/src/main/java/ro/itec/waity/login/view/LoginActivityView.java
+++ app/src/main/java/ro/itec/waity/login/view/LoginActivityView.java
@@ -1,14 +1,18 @@
 package ro.itec.waity.login.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import ro.itec.waity.R;
 import ro.itec.waity.login.LoginMVP;
+import ro.itec.waity.table.backend.NFCEventListener;
+import ro.itec.waity.table.backend.NFCManager;
 
-public class LoginActivityView extends AppCompatActivity implements LoginMVP.RequiredViewOps {
+public class LoginActivityView extends AppCompatActivity implements LoginMVP.RequiredViewOps, NFCEventListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -16,6 +20,54 @@ public class LoginActivityView extends AppCompatActivity implements LoginMVP.Req
 
         setContentView(R.layout.activity_login);
         setStatusBarTranslucent(true);
+
+        // Initialise the NFC manager to make the application ready to read NFC tags
+        NFCManager.INSTANCE.init(this);
+
+        // Check for intent type and decide if it's NFC related or not
+        NFCManager.INSTANCE.handleIntent(this, getIntent());
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        /**
+         * It's important, that the activity is in the foreground (resumed). Otherwise
+         * an IllegalStateException is thrown.
+         */
+        NFCManager.INSTANCE.setupForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        /**
+         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
+         */
+        NFCManager.INSTANCE.stopForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        /**
+         * This method gets called, when a new Intent gets associated with the current activity instance.
+         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
+         * at the documentation.
+         *
+         * In our case this method gets called, when the user attaches a Tag to the device.
+         */
+        NFCManager.INSTANCE.handleIntent(this, intent);
+    }
+
+    @Override
+    public void onNFCReadCompleted(boolean success, String nfcString) {
+        if (success) {
+            Toast.makeText(this, nfcString, Toast.LENGTH_LONG).show();
+        }
     }
 
     protected void setStatusBarTranslucent(boolean makeTranslucent) {
