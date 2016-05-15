6ee90a76e1fe3aa6ebd5b196cd7c570cf541627e
Norbert <balazsnorbi@gmail.com>
2016-05-15 06:23:59 +0300

    Desk ID

--- app/src/main/java/ro/itec/waity/order/view/NavMenuFragment.java
+++ app/src/main/java/ro/itec/waity/order/view/NavMenuFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.mxn.soul.flowingdrawer_core.MenuFragment;
 
@@ -24,8 +26,20 @@ public class NavMenuFragment extends MenuFragment {
    @Override
    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       View view = inflater.inflate(R.layout.fragment_menu, container, false);
-      TextView textView = (TextView)view.findViewById(R.id.nav_draw_user_name);
-      textView.setText(PreferencesMgr.INSTANCE.readString(KeyList.KEY_USER_NAME));
+
+      TextView userName = (TextView)view.findViewById(R.id.nav_draw_user_name);
+      userName.setText(PreferencesMgr.INSTANCE.readString(KeyList.KEY_USER_NAME));
+
+      TextView tableID = (TextView)view.findViewById(R.id.tv_tableID);
+      tableID.setText("" + PreferencesMgr.INSTANCE.readInt(KeyList.KEY_DESK_ID));
+
+      Button button = (Button)view.findViewById(R.id.btn_logout);
+      button.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            Toast.makeText(getContext(), "Logout nou!", Toast.LENGTH_SHORT).show();
+         }
+      });
 
       return setupReveal(view);
    }
